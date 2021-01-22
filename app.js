e6a0b795eec4159e17dd75af669d04a8c8b7974c
Ransom Ahanmisi <60149483+Ransom8@users.noreply.github.com>
2021-01-23 00:15:07 +0100

    Ready to deploy

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const superagent = require('superagent');
+const AppError = require('./utils/appError');
+
+const app = express();
+
+app.get('/api/rates', async (req, res, next) => {
+
+  try {
+
+    let obj = {};
+    const {
+      base,
+      currency
+    } = req.query;
+
+    const {
+      body: {
+        rates
+      }
+    } = await superagent(`https://api.exchangeratesapi.io/latest?base=${base}`);
+
+    currency.split(',').forEach(el => {
+      if (el in rates) {
+        obj[el] = rates[el]
+      }
+    });
+
+
+    res.status(200).json({
+      result: {
+        "base": base,
+        "date": new Date(Date.now()).toISOString().slice(0, 10),
+        "rates": obj
+      }
+    })
+
+  } catch (err) {
+    return next(new AppError(err.message, 400))
+  }
+});
+
+app.all('*', (req, res, next) => {
+  throw next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use((err, req, res, next) => {
+
+  throw res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+})
+
+app.listen(3000, () => console.info("Server connected successfully!"));
